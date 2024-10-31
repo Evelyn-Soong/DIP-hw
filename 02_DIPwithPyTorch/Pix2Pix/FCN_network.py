55701ea2b2dee297a346f640f04aff26d306c7c9
Evelyn-Soong <soungwentian@mail.ustc.edu.cn>
2024-10-31 23:09:48 +0800

    Update FCN_network.py

--- 02_DIPwithPyTorch/Pix2Pix/FCN_network.py
+++ 02_DIPwithPyTorch/Pix2Pix/FCN_network.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class FullyConvNetwork(nn.Module):
@@ -12,46 +13,108 @@ class FullyConvNetwork(nn.Module):
         )
         ### FILL: add more CONV Layers
         self.conv2 = nn.Sequential(
-            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),  # Input channels: 8, Output channels: 16
-            nn.BatchNorm2d(16),
-            nn.ReLU(inplace=True)
+            nn.Conv2d(8, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, True)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),  # Input channels: 16, Output channels: 32
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True)
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True)
         )
 
         # Decoder (Deconvolutional Layers)
         ### FILL: add ConvTranspose Layers
         ### None: since last layer outputs RGB channels, may need specific activation function
         self.deconv1 = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),  # Input channels: 32, Output channels: 16
-            nn.BatchNorm2d(16),
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True)
         )
         self.deconv2 = nn.Sequential(
-            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),  # Input channels: 16, Output channels: 8
-            nn.BatchNorm2d(8),
+            nn.ConvTranspose2d(512 + 512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.Dropout2d(0.5),
             nn.ReLU(inplace=True)
         )
         self.deconv3 = nn.Sequential(
-            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),  # Input channels: 8, Output channels: 3
-            nn.Sigmoid()  # Output channels: 3, Sigmoid for RGB values in range [0, 1]
+            nn.ConvTranspose2d(512 + 512, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.Dropout2d(0.5),
+            nn.ReLU(inplace=True)
         )
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(256 + 256, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.deconv5 = nn.Sequential(
+            nn.ConvTranspose2d(128 + 128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.deconv6 = nn.Sequential(
+            nn.ConvTranspose2d(64 + 64, 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(inplace=True)
+        )
+        self.last_Conv = nn.Sequential(
+            nn.ConvTranspose2d(8 + 8, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
+
+        self.init_weight()
+
+    def init_weight(self):
+        for w in self.modules():
+            if isinstance(w, nn.Conv2d):
+                nn.init.kaiming_normal_(w.weight, mode='fan_out')
+                if w.bias is not None:
+                    nn.init.zeros_(w.bias)
+            elif isinstance(w, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(w.weight, mode='fan_in')
+            elif isinstance(w, nn.BatchNorm2d):
+                nn.init.ones_(w.weight)
+                nn.init.zeros_(w.bias)
 
     def forward(self, x):
         # Encoder forward pass
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = self.conv3(out)
+        down1 = self.conv1(x)
+        down2 = self.conv2(down1)
+        down3 = self.conv3(down2)
+        down4 = self.conv4(down3)
+        down5 = self.conv5(down4)
+        down6 = self.conv6(down5)
+        down7 = self.conv7(down6)
 
         # Decoder forward pass
-        out = self.deconv1(out)
-        out = self.deconv2(out)
-        output = self.deconv3(out)
-        ### FILL: encoder-decoder forward pass
-
+        up1 = self.deconv1(down7)
+        up2 = self.deconv2(torch.cat([up1, down6], dim=1))
+        up3 = self.deconv3(torch.cat([up2, down5], dim=1))
+        up4 = self.deconv4(torch.cat([up3, down4], dim=1))
+        up5 = self.deconv5(torch.cat([up4, down3], dim=1))
+        up6 = self.deconv6(torch.cat([up5, down2], dim=1))
+        output = self.last_Conv(torch.cat([up6, down1], dim=1))
         
         return output
     
